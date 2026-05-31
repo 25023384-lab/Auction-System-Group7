58f8d8a82b64ce32d0cba1968c7901d12a34333a
Tuan4 <25023358@vnu.edu.vn>
2026-05-31 23:02:31 +0700

    Sửa jar

--- src/main/java/com/auction/app/Main.java
+++ src/main/java/com/auction/app/Main.java
@@ -1,14 +1,21 @@
 package com.auction.app;
 
-import com.auction.server.AuctionServer;
 import com.auction.client.AuctionClient;
+import com.auction.server.AuctionServer;
 
 public class Main {
+    public static class Launcher {
+        public static void main(String[] args) {
+            Main.main(args);
+        }
+    }
 
     public static void main(String[] args) {
-        if (args.length > 0 && "server".equals(args[0])) {
+        if (args.length > 0 && "server".equalsIgnoreCase(args[0])) {
+
             AuctionServer.main(args);
         } else {
+
             AuctionClient.main(args);
         }
     }
